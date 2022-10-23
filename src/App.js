32f0c3774af8bddebf35649c5731a18b4b8c2dca
afzalhossain00 <afzalhossain.workspace@gmail.com>
2022-10-23 21:33:43 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import { Spinner } from 'react-bootstrap';
+import GridCard from './components/GridCard/GridCard';
+import NavBar from './components/NavBar/NavBar';
+import ShowToast from './components/ShowToast/ShowToast';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar></NavBar>
+      <GridCard></GridCard> <br />
+      <ShowToast></ShowToast> <br />
+      <Spinner animation="grow" variant="success" />
     </div>
   );
 }
